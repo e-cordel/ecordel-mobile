930f8f504ea6d3d891012b89ecdf4022f14b36fc
Regis Brilhante <regis@pop-os.localdomain>
2020-10-26 18:15:25 -0300

    organizaçao e limpeza do codigo

--- lib/models/ecordel.dart
+++ lib/models/ecordel.dart
@@ -8,14 +8,9 @@ class Ecordel with ChangeNotifier {
   String title;
   String description;
   String content;
-  String xilogravura; // url
+  String xilogravura;
 
   List<String> tags;
-  //  = [
-  //   "engraçado",
-  //   "regional",
-  //   "regionais",
-  // ];
 
   Ecordel({
     this.id,
@@ -26,28 +21,6 @@ class Ecordel with ChangeNotifier {
     this.xilogravura,
     this.tags,
   });
-
-  // factory Ecordel.fromJson(Map<String, dynamic> json) {
-  //   return Ecordel(
-  //     id: json['id'],
-  //     author: json['author'] as String,
-  //     tittle: json['title'] as String,
-  //     content: json['content'] as String,
-  //     description: json['description'] as String,
-  //     xilogravura: json['xilogravura'] as String,
-  //   );
-  // }
-
-  // Map<String, dynamic> toJson() {
-  //   return {
-  //     'id': this.id,
-  //     'author': this.author,
-  //     'tittle': this.tittle,
-  //     'content': this.content,
-  //     'description': this.description,
-  //     'xilogravura': this.xilogravura,
-  //   };
-  // }
 
   Map<String, dynamic> toMap() {
     return {
