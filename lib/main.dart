c19f891f78ec42e257e8c7984a0bf2bbee150849
Regis Brilhante <regis@pop-os.localdomain>
2020-10-26 18:16:31 -0300

    removido temporariamente google fonts do tema da aplicacao

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:ecordel/providers/cordel_provider.dart';
+import 'package:ecordel/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:google_fonts/google_fonts.dart';
-import './screens/cordel_overview_screen.dart';
 
 void main() => runApp(MultiProvider(
       providers: [
@@ -14,26 +13,12 @@ void main() => runApp(MultiProvider(
     ));
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          appBarTheme: AppBarTheme(
-              textTheme: TextTheme().copyWith(
-                  headline6: TextStyle(
-                      fontSize: 25,
-                      fontFamily: GoogleFonts.pangolin().fontFamily,
-                      color: Colors.black))),
-          brightness: Brightness.light,
-          primarySwatch: Colors.amber,
-          backgroundColor: Colors.amber[100],
-          accentColor: Colors.amberAccent[700],
-          fontFamily: GoogleFonts.pangolin().fontFamily,
-          textTheme: TextTheme()),
-      home: CordelOverviewScreen(),
+      title: 'E-Cordel',
+      home: HomeScreen(),
     );
   }
 }
