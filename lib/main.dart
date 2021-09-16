7249cecbc33555195f2d8bd4eeb4a953556e40dc
Regis Brilhante <regis.f.brilhante@gmail.com>
2021-09-15 21:03:32 -0300

    recria HomeScreen e app inicial do aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,20 @@ import 'package:ecordel/app/app_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:ecordel/app/app_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import './screens/home.dart';
 
-void main() => runApp(ModularApp(module: AppModule(), child: AppWidget()));
+// void main() => runApp(ModularApp(module: AppModule(), child: AppWidget()));
+void main() => runApp(EcordelApp());
+
+class EcordelApp extends StatelessWidget {
+  const EcordelApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Cordel',
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: HomeScreen(),
+    );
+  }
+}
